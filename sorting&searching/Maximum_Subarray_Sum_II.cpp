55daa90aa14a6acf2aa4a7414a66965cf0653862
aaryadixit <dixitaarya3@gmail.com>
2026-07-21 23:01:37 +0530

    2 ques

---
+++ sorting&searching/Maximum_Subarray_Sum_II.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define vvi vector<vector<int>>
+#define vll vector<ll>
+#define pll pair<long,long>
+#define vpll vector<pll>
+#define vvll vector<vector<ll>>
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define take(arr, n) \
+    vector<ll> arr(n); \
+    for(auto &it : arr) cin >> it
+const ll M = 1000000007;
+#define le(x) \
+    ll x; \
+    cin >> x
+#define prnt(a) cout << a << endl
+
+/*---------------------------------*/
+string abc = "abcdefghijklmnopqrstuvwxyz";
+const ll N = 100005;
+int dp[1000005];
+int dr[4] = {-1, 1, 0, 0};
+int dc[4] = {0, 0, -1, 1};
+
+class FenwickTree {
+private:
+    vector<long long> bit;
+    int n;
+
+public:
+    FenwickTree(int size) {
+        n = size;
+        bit.assign(n + 1, 0);
+    }
+
+    void update(int index, long long delta) {
+        while(index <= n) {
+            bit[index] += delta;
+            index += index & -index;
+        }
+    }
+
+    long long query(int index) {
+        long long sum = 0;
+        while(index > 0) {
+            sum += bit[index];
+            index -= index & -index;
+        }
+        return sum;
+    }
+
+    long long rangeQuery(int left, int right) {
+        return query(right) - query(left - 1);
+    }
+};
+/*---------------------------------*/
+
+void func(){
+   
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, a, b;
+    cin>>n>>a>>b;
+    vll arr(n+1);
+    for(int i=1; i<=n; ++i) cin >> arr[i];
+    vll prefix(n+1, 0);
+    for(int i=1; i<=n; ++i){
+        prefix[i] = prefix[i-1] + arr[i];
+    }
+    ll ans=LLONG_MIN;
+    multiset<ll> s;
+    for(int i{a}; i<=n; ++i){
+        s.insert(prefix[i-a]);
+        
+        if(i-b-1 >= 0){
+            s.erase(s.find(prefix[i-b-1]));
+        }
+        ans = max(ans, prefix[i] - *s.begin());
+    }
+    
+    prnt(ans);
+}
